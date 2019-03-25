29b9d154fadccd0605143e451c9ba774f97a8056
Gabriel <eu@gabrielf.com>
2019-03-25 16:41:10 -0300

    Loading helpers

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 ini_set('display_errors', 1);
 
 require_once dirname(__FILE__) . '/../vendor/autoload.php';
-
+require_once dirname(__FILE__) . '/../src/helpers.php';
 require_once dirname(__FILE__) . '/../config.php';
 $authenticated = false;
 
